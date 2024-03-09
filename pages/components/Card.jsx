d34ef18ab1842fd50652208627216dc0ad6071ec
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-09 01:41:25 -0300

    Card Component

---
+++ pages/components/Card.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from '../../styles/Card.module.css'
+
+export const Card = ({ pokemon }) => {
+    return (
+        <div className={styles.card}>
+            <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} width="120" height="120" alt={pokemon.name}/>
+            <p className={styles.id}>#{pokemon.id}</p>
+            <h3 className={styles.title}>{pokemon.name}</h3>
+            <Link href={`/pokemon/${pokemon.id}`} className={styles.link}>Details</Link>
+        </div>
+    )
+}
