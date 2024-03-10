85505decbddff7882e950225bdb56815a733a710
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-09 23:16:17 -0300

    Details Page Layout

--- pages/pokemon/[pokemonId].jsx
+++ pages/pokemon/[pokemonId].jsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+
+import styles from '../../styles/Pokemon.module.css'
+
 export const getStaticPaths = async() => {
     const maxPokemons = 251;
     const api = 'http://pokeapi.co/api/v2/pokemon/';
@@ -30,7 +34,34 @@ export const getStaticProps = async(context) => {
 
 export const Pokemon = ({ pokemon }) => {
     return (
-        <p>{pokemon.name}</p>
+        <div className={styles.pokemon_container}>
+            <h1 className={styles.title}>{pokemon.name}</h1>
+            <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} width="120" height="120" alt={pokemon.name}/>
+            <div>
+                <h3>Number: </h3>
+                <p>#{pokemon.id}</p>
+            </div>
+            <div>
+                <h3>Type: </h3>
+                <div className={styles.types_container}>
+                    {pokemon.types.map((item, index) => (
+                        <span key={index} className={`${styles.type} ${styles['type_' + item.type.name]}`}>
+                            {item.type.name}
+                        </span>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.data_container}>
+                <div className={styles.data_height}>
+                    <h4>Height: </h4>
+                    <p>{pokemon.height * 10} cm</p>
+                </div>
+                <div className={styles.data_weight}>
+                    <h4>Weight: </h4>
+                    <p>{pokemon.weight / 10} kg</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
